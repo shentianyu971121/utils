8db9140d69294c6ccf817e487c0532a3f833e502
moteinstorm <454665830@qq.com>
2019-11-08 11:27:16 +0800

    添加异常

---
+++ src/main/java/com/zhuzhiguang/CmsException.java
@@ -0,0 +1,20 @@
+package com.zhuzhiguang;
+
+/**
+ * 自定义异常
+ * @author zhuzg
+ *
+ */
+public class CmsException  extends Exception{
+
+	public CmsException(String string) {
+		// TODO Auto-generated constructor stub
+		super(string);
+	}
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -2388326871380439088L;
+
+}
