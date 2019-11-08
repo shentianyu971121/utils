7458a4f55db6c702393f0f4084ce9cc8b08c52de
moteinstorm <454665830@qq.com>
2019-11-08 11:19:06 +0800

    增加日期工具类中 月初、月末 等等方法

--- src/test/java/com/bw/test/TestDate.java
+++ src/test/java/com/bw/test/TestDate.java
@@ -1,5 +1,7 @@
 package com.bw.test;
 
+import static org.junit.Assert.assertTrue;
+
 import java.util.Date;
 
 import org.junit.Test;
@@ -12,5 +14,31 @@ public class TestDate {
 	public void testDate() {
 		DateUtils.compare(null, new Date());
 	}
+	
+	
+	@Test
+	public void testAge() {
+		
+		Date date = new Date();
+		assertTrue(0==DateUtils.calculateAge(date));
+		
+		
+		Date date2 = new Date(-12,11,9);
+		int age = DateUtils.calculateAge(date2);
+		System.out.println(" age shi " + age);	
+	}
+	
+	@Test
+	public void testBom() {
+		Date  bom = DateUtils.getBOM(new Date());
+		System.out.println("bom is " + bom);
+	}
+	
+	@Test
+	public void testEom(){
+		Date  eom = DateUtils.getEOM(new Date());
+		System.out.println("eom is " + eom);
+	}
 
+	
 }
