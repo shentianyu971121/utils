0c818d0b8c43d7fbe1ba31452ef1d5963bb97c53
shentianyu971121 <948653027@qq.com>
2019-11-09 08:33:01 +0800

    第二次上传

--- src/test/java/com/bw/test/TestStringUtils.java
+++ src/test/java/com/bw/test/TestStringUtils.java
@@ -11,20 +11,15 @@ public class TestStringUtils {
 	
 	@Test
 	public void testIsBlank() {
-		
-		
 		// 断言  真
 		assertTrue( StringUtils.isBlank("   "));
-		
 	}
-	
 	
 	@Test
 	public void testIsBlank2() {
 		//断言 假
 		assertFalse( StringUtils.isBlank("aaaa"));
 	}
-	
 	@Test
 	public void testRandomn() {
 		String s = StringUtils.getRandomStr(10);
@@ -36,8 +31,6 @@ public class TestStringUtils {
 		String s = StringUtils.getRandomStr2(25);
 		System.out.println(" s is " + s + "  and 长度是  " + s.length());
 	}
-	
-	
 	@Test
 	public void testCnStrn() {
 		String s = StringUtils.getRandomCn(25);
